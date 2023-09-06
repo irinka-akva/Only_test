23e7c313dfa1e19e9d8651b278d348f56c77a50f
Irina <irinka-akva@ya.ru>
2023-09-06 17:01:54 +0300

    Добавляет стартовые файлы для задания

---
+++ js/main.js
@@ -0,0 +1,24 @@
+// Cделать, чтобы при нажатии кнопки "Кнопка 1" скрывался блок с заголовком. При повторном нажатии блок должен появляться.
+
+const button_1 = document.querySelector(".btn-1");
+const container_1 = document.querySelector(".inner-container-1");
+
+button_1.addEventListener('click', () => container_1.classList.toggle("inner-container-1-js"));
+
+
+// При нажатии кнопки "Кнопка 2" средний блок во втором ряду должен меняться местами с левым блоком. При повторном нажатии 
+// - возвращаться на прежнее место.
+
+const button_2 = document.querySelector(".btn-2");
+const container_3 = document.querySelector(".inner-container-3");
+
+button_2.addEventListener('click', () => container_3.classList.toggle("inner-container-3-js"));
+
+
+// Сделать, чтобы при открытии страницы появлялось модальное окно с надписью "Привет, мир".
+
+const modal = new bootstrap.Modal(document.querySelector('#exampleModal'));
+
+window.addEventListener('load', function () {
+  modal.show();
+});

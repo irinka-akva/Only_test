67eb40605953db37cc87509c7ebc4de54dadf2de
Irina <irinka-akva@ya.ru>
2023-09-07 19:54:33 +0300

    Завершает верстку страницы, добавляет скрипт

--- js/main.js
+++ js/main.js
@@ -1,24 +1,10 @@
-// Cделать, чтобы при нажатии кнопки "Кнопка 1" скрывался блок с заголовком. При повторном нажатии блок должен появляться.
+const buttons = document.querySelectorAll(".btn");
 
-// const button_1 = document.querySelector(".btn-1");
-// const container_1 = document.querySelector(".inner-container-1");
-
-// button_1.addEventListener('click', () => container_1.classList.toggle("inner-container-1-js"));
-
-
-// // При нажатии кнопки "Кнопка 2" средний блок во втором ряду должен меняться местами с левым блоком. При повторном нажатии 
-// // - возвращаться на прежнее место.
-
-// const button_2 = document.querySelector(".btn-2");
-// const container_3 = document.querySelector(".inner-container-3");
-
-// button_2.addEventListener('click', () => container_3.classList.toggle("inner-container-3-js"));
-
-
-// // Сделать, чтобы при открытии страницы появлялось модальное окно с надписью "Привет, мир".
-
-// const modal = new bootstrap.Modal(document.querySelector('#exampleModal'));
-
-// window.addEventListener('load', function () {
-//   modal.show();
-// });
+document.addEventListener('click', (e) => {
+    const target = e.target;
+    if (!target.closest(".btn")) {
+        buttons.forEach(button => button.classList.remove("open-js"));
+    } else if (target.closest(".btn")) {
+        target.classList.toggle("open-js");
+    }
+})
